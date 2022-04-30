bb7c5379c915ccd248185b7c9076a0a9acd04df4
Sepehr <sepehr.golestanian@gmail.com>
2022-04-30 13:18:48 +0430

    Updated score.py

--- template_code/score.py
+++ template_code/score.py
@@ -1,16 +1,39 @@
+import sys, getopt
 import pandas as pd
 
-# Import results
-results_table = pd.read_excel('results.xlsx')
 
-# count true positives, true negatives, false positives and false negatives
-confmat = (
-    results_table
-    .groupby(['label', 'classification'])
-    .count()
-    .unstack()
-)
+def main(argv):
 
-# calculate scores
+    # Parse arguments
+    input_file, output_file = arg_parser(argv)
 
-# write results to file
+    # Import results
+    results_table = pd.read_excel(input_file)
+
+    # count true positives, true negatives, false positives and false negatives
+    confmat = (
+        results_table
+        .groupby(['label', 'classification'])
+        .count()
+        .unstack()
+    )
+
+    print(confmat)
+    # calculate scores
+
+    # write results to file
+    
+
+def arg_parser(argv):
+    opts, args = getopt.getopt(argv,"i:o:",["ifile=","ofile="])
+
+    for opt, arg in opts:
+        if opt == '-i':
+            input_file = arg
+        elif opt == '-o':
+            output_file = arg
+    return input_file, output_file
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
