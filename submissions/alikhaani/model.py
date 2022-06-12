2461cfa3132680a6a3026c2af0bd097dadff18a2
Sepehr <sepehrgolestanian@icloud.com>
2022-06-12 16:20:16 +0430

    Updated model.py

--- submissions/alikhaani/model.py
+++ submissions/alikhaani/model.py
@@ -3,6 +3,7 @@ from tensorflow.keras.models import load_model
 from scipy.io import loadmat
 # Please run "pip install neurokit2" to install NeuroKit2
 import neurokit2.signal as nsig
+import numpy as np
 
 class AFDetector:
     """
@@ -17,13 +18,12 @@ class AFDetector:
         sample_rate: int
             Sampling frequency of the inputs.
         """
-
+        self.default_rate = sample_rate
 
         # load model
         self.model = load_model("AF-detection.h5")
 
         # load filters
-        self.sample_rate = sample_rate
 
     def classify(self, ecg):
         """
@@ -44,30 +44,42 @@ class AFDetector:
         """
         preprocessed_ecg = self.preprocess(ecg)
         model_classifications = self.get_model_classification(preprocessed_ecg)
-        return tf.argmax(model_classifications)
+        return tf.squeeze(tf.argmax(model_classifications))
 
     def preprocess(self, ecg):
         """
         Gets the ecg signal and prepares it for classification. Filtering,
         windowing and etc can be done here.
         """
-        default_fs = 360            
 
-        if self.sample_rate != default_fs:
-            s = nsig.signal_resample(ecg,sampling_rate=self.sample_rate,desired_sampling_rate=360)
-            ecg = s[1]
+        sys_fs = 360
+
+        if sys_fs != self.default_rate:
+            s = nsig.signal_resample(ecg,sampling_rate=self.default_rate,desired_sampling_rate=360 )
+            ecg = s
 
         maxlen = 2160
 
         if len(ecg) > maxlen:
             sig_len = len(ecg)
             new_len = sig_len - maxlen
-            sig = sig[new_len:]
+            sig = ecg[new_len:]
+        elif len(ecg) < maxlen:
+            sig = self.fit_tolength(ecg,maxlen)
+        elif len(ecg) == maxlen:
+            sig = ecg
+            
 
         sig2 = sig.reshape(1,2160)
         sig2 = sig2.astype('float32')
 
         return sig2
+
+    def fit_tolength(source, length):
+        target = np.zeros([length])
+        w_l = min(source.size, target.size)
+        target[0:w_l] = source[0:w_l]
+        return target
 
 
     def get_model_classification(self, ecg):
